aca40a23befba3211febe490116f63ff024f59c2
lina-22 <haquefahinur@gmail.com>
2023-11-15 23:13:17 +0100

    udating the ngModel

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Todo } from './model';
+// import { NgModel } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  todoList: Todo[] = [];
+  todo: Todo = {
+    Title: '',
+    Id: null,
+  };
+
+  addTodo(): void {
+    console.log(this.todo);
+  }
 }
